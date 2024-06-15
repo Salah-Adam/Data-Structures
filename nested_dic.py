f8f8503569571f4d3c6e0acb904023a3c885d1c2
salahAI <salah21@gmail.com>
2024-06-15 13:08:08 +0300

    add nested_dic file

---
+++ nested_dic.py
@@ -0,0 +1,20 @@
+employees={
+    "emp1":{
+        "name": "Ahmed Ali", "age":24, "salary": "$300"
+    },
+    "emp2":{
+        "name": "Ali Omar", "age":26, "salary": "$350"
+    },
+    "emp3":{
+        "name": "Mohamed Isack", "age":30, "salary": "$500"
+    },
+    "emp4":{
+        "name": "Fahima Abdi", "age":23, "salary": "$300"
+    },
+    "emp5":{
+        "name": "Osman Ibrahim", "age":35, "salary": "$500"
+    },
+}
+print(employees)
+print(employees["emp3"])
+print(employees["emp4"]["salary"])
